99869bb0256e966d6a18c822dbe958fefb2c1621
wahhab <wahhabawaludin2004@mail.ugm.ac.id>
2024-10-07 11:21:21 +0700

    Initial

--- app/src/main/java/com/example/ujianadasolusi/SecondActivity.kt
+++ app/src/main/java/com/example/ujianadasolusi/SecondActivity.kt
@@ -1,4 +1,29 @@
 package com.example.ujianadasolusi
 
-class SecondActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ujianadasolusi.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+
+            btnToHome.setOnClickListener {
+
+                val intent = Intent(this@SecondActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+    }
 }
